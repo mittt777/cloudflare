bfa88b48956708000645a245be8c607940e24fa3
mittt777 <121394585+mittt777@users.noreply.github.com>
2025-02-26 19:31:12 +0800

    Delete drizzle.config.ts

--- drizzle.config.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  dialect: "postgresql",
-  schema: "./src/schema/transaction.ts",
-  out: "./migrations",
-});
